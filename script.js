40efe38989bf7bf18f424ce853b01f2be03a0c08
JoseAlonsoDev <alonsomvdev@gmail.com>
2025-11-06 21:42:59 -0300

    Second commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+let nombre = "Jose";
+
+let edad = 24;
+
+let soyEstudiante = false;
+
+if (soyEstudiante == true) {
+    soyEstudiante = "soy estudiante"
+} else {
+    soyEstudiante = "no soy estudiante"
+}
+
+console.log(`Mi nombre es ${nombre}, mi edad es ${edad} y ${soyEstudiante}`);
